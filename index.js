cc8fe85e925a49f64dd8742a2c217fd415a3d909
Rahul Goyal <rahul@geeksforgeeks.org>
2024-01-09 16:34:42 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,9 +1,62 @@
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// })
+
+app.get("/expenses", (req, res) => {
+    fs.readFile("./data.json", "utf-8", (err, data) => {
+        // console.log(data);
+        res.send(data);
+    })
 })
+
+app.get("/expenses/:id", (req, res) => {
+    fs.readFile("./data.json", "utf-8", (err, data) => {
+        let formattedData = JSON.parse(data);
+        // let formattedData = JSON.stringify(data);
+        let item = formattedData.find(element => element.id === parseInt(req.params.id));
+        
+        if (!item) {
+            res.status(404);
+            return res.send({ message: "Data not exist" });
+        }
+
+        res.send(item);
+    })
+})
+
+app.post("/expenses", (req, res) => {
+    const payload = req.body;
+    console.log(payload);
+
+    res.send({ message: "SUCCESS" })
+})
+
+app.put("/expenses/:id", (req, res) => {
+    const payload = req.body;
+    const id = req.params.id;
+    console.log(payload, id);
+
+    res.send({ message: "SUCCESS" })
+})
+
+app.delete("/expenses/:id", (req, res) => {
+    const id = req.params.id;
+    console.log(`Data to be deleted: ${id}`);
+
+    res.send({ message: "SUCCESS" })
+})
+
 
 const PORT = 8000;
 app.listen(PORT, () => {
---
+++ resources.md
@@ -0,0 +1,6 @@
+Body Parser: https://expressjs.com/en/resources/middleware/body-parser.html
+CORS: https://www.npmjs.com/package/cors
+Nodemon: https://www.npmjs.com/package/nodemon
+Express Routing: https://expressjs.com/en/guide/routing.html
+Express: https://expressjs.com/
+Node: https://nodejs.org/en/learn/getting-started/introduction-to-nodejs
