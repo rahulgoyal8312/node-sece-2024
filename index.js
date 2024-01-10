53049e16ce5a73bf2f7db377050457dd5f728a52
Rahul Goyal <rahul@geeksforgeeks.org>
2024-01-10 10:48:11 +0530

    MongoDB added and APIs updated

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+const mongoose = require("mongoose");
+
+mongoose.connect(
+    `mongodb+srv://root:q5fr2dofN2giWZ8k@mern-2024.0bpg7af.mongodb.net/?retryWrites=true&w=majority`, 
+    {
+        dbName: "expense-tracker"
+    }
+).then(() => {
+    console.log("Successfully connected to MongoDB");
+})
+.catch(error => {
+    console.log("Error connecting to MongoDB", error);
+})
+
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -13,46 +27,72 @@ app.use(cors());
 //     res.send('Hello World!');
 // })
 
-app.get("/expenses", (req, res) => {
-    fs.readFile("./data.json", "utf-8", (err, data) => {
-        // console.log(data);
-        res.send(data);
-    })
+const ExpenseModel = mongoose.model('expenses', {
+    title: String,
+    description: String,
+    amount: Number,
+    type: Number,
+    date: String
+});
+
+
+app.get("/expenses", async (req, res) => {
+    const data = await ExpenseModel.find();
+    return res.send(data);
+    // fs.readFile("./data.json", "utf-8", (err, data) => {
+    //     // console.log(data);
+    //     res.send(data);
+    // })
 })
 
-app.get("/expenses/:id", (req, res) => {
-    fs.readFile("./data.json", "utf-8", (err, data) => {
-        let formattedData = JSON.parse(data);
-        // let formattedData = JSON.stringify(data);
-        let item = formattedData.find(element => element.id === parseInt(req.params.id));
+app.get("/expenses/:id", async (req, res) => {
+
+    const data = await ExpenseModel.findById(req.params.id);
+    return res.send(data);
+
+    // fs.readFile("./data.json", "utf-8", (err, data) => {
+    //     let formattedData = JSON.parse(data);
+    //     // let formattedData = JSON.stringify(data);
+    //     let item = formattedData.find(element => element.id === parseInt(req.params.id));
         
-        if (!item) {
-            res.status(404);
-            return res.send({ message: "Data not exist" });
-        }
+    //     if (!item) {
+    //         res.status(404);
+    //         return res.send({ message: "Data not exist" });
+    //     }
 
-        res.send(item);
-    })
+    //     res.send(item);
+    // })
 })
 
-app.post("/expenses", (req, res) => {
+app.post("/expenses", async (req, res) => {
     const payload = req.body;
     console.log(payload);
 
+    const expense = new ExpenseModel(payload);
+    const item = await expense.save();
+
+    console.log("POST OPERATION, ITEM: ", item);
+
     res.send({ message: "SUCCESS" })
 })
 
-app.put("/expenses/:id", (req, res) => {
+app.put("/expenses/:id", async (req, res) => {
     const payload = req.body;
     const id = req.params.id;
-    console.log(payload, id);
+    console.log("PUT API: ", payload, id);
+
+    const item = await ExpenseModel.findByIdAndUpdate(id, payload);
+    console.log(item);
 
     res.send({ message: "SUCCESS" })
 })
 
-app.delete("/expenses/:id", (req, res) => {
+app.delete("/expenses/:id", async (req, res) => {
     const id = req.params.id;
     console.log(`Data to be deleted: ${id}`);
+
+    const item = await ExpenseModel.findByIdAndDelete(id);
+    console.log(item);
 
     res.send({ message: "SUCCESS" })
 })
--- resources.md
+++ resources.md
@@ -1,6 +1,30 @@
+# Session 1:
+----------
 Body Parser: https://expressjs.com/en/resources/middleware/body-parser.html
 CORS: https://www.npmjs.com/package/cors
 Nodemon: https://www.npmjs.com/package/nodemon
 Express Routing: https://expressjs.com/en/guide/routing.html
 Express: https://expressjs.com/
 Node: https://nodejs.org/en/learn/getting-started/introduction-to-nodejs
+
+
+# Session 2:
+----------
+MongoDB installation: https://www.prisma.io/dataguide/mongodb/setting-up-a-local-mongodb-database
+MongoDB official documentation: https://www.mongodb.com/
+Mongoose: https://mongoosejs.com/
+
+
+
+Username: admin
+Pass: ZmTYtTUUQWGC42Ju
+
+
+
+const mongoose = require('mongoose');
+module.exports = function () {
+    mongoose.connect('mongodb+srv://admin:2E2KtfgBCygDzrZF@rg.tbcbpkc.mongodb.net/', {
+        dbName: "mern-2024"
+    }).then(() => console.log(`Connected to MongoDB`))
+        .catch(ex => console.error(ex))
+}
